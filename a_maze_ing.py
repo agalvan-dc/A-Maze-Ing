a133cc1c6951bf45007b8537052140ade76a34fa
agalvan-dc <a.galvandecastro@gmail.com>
2026-07-21 22:01:14 +0200

    .txt, parsing, mlx and other stuff

---
+++ a_maze_ing.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+
+import numpy as np
+
+def main() -> None:
+    print()
+
+
+if __name__ == "__main__":
+    main()
---
+++ parsing.py
@@ -0,0 +1,59 @@
+
+from pydantic import BaseModel, model_validator, field_validator, Field
+from pydantic import ValidationError
+
+
+class ValidateConfig(BaseModel):
+    width: int = Field(ge=1)
+    height: int = Field(ge=1)
+    entry: tuple[int, int]
+    exit: tuple[int, int]
+    output_file: str = Field(min_length=5)
+    perfect: bool = Field(default=True)
+
+    @field_validator('entry', 'exit', mode='before')
+    @classmethod
+    def parse_tuple(cls, value: str) -> tuple[int, int] | str:
+        if isinstance(value, str):
+            try:
+                x, y = value.split(',')
+                return (int(x.strip()), int(y.strip()))
+            except ValueError:
+                raise ValueError("Format must be: 'x,y' (ej: '2,3')")
+        return value
+
+    @model_validator(mode='after')
+    def verify(self) -> 'ValidateConfig':
+        entry_x, entry_y = self.entry
+        if entry_x >= self.width or entry_y >= self.height:
+            raise ValueError(f"Entry point {self.entry} "
+                             "outside of maze dimensions "
+                             f"({self.width}x{self.height})")
+
+        exit_x, exit_y = self.exit
+        if exit_x >= self.width or exit_y >= self.height:
+            raise ValueError(f"Exit point {self.exit} outside of maze "
+                             f"dimensions ({self.width}x{self.height})")
+
+        return self
+
+
+def validate(filepath: str) -> dict:
+    config_dict = {}
+
+    try:
+        with open(filepath, 'r', encoding='utf-8') as file:
+            for line in file:
+                line = line.strip()
+                if not line or '=' not in line:
+                    continue
+
+                key, value = line.split('=', 1)
+                config_dict[key.strip().lower()] = value.strip()
+
+        config = ValidateConfig(**config_dict)
+        return config.model_dump()
+    except FileNotFoundError:
+        print(f"Error: File not found '{filepath}'")
+    except ValidationError as e:
+        print(f"Validation error: {e}")
