b4adb99dc47bb24a5297c49add12ab57d69affd5
Carlos Perales Sanchez <caperale@c1r17s2.42madrid.com>
2026-07-22 18:20:17 +0200

    maze generator things

--- structure/maze_generator.py
+++ structure/maze_generator.py
@@ -1,17 +1,33 @@
 import numpy as np
 from pathlib import Path
+import random
 
 
-def generate_maze(input: dict) -> str:
+def generate_maze(input: str) -> str:
+    config_dict = {}
+    with open(input, "r", encoding='utf-8') as config:
+        for line in config:
+            if not line.strip():
+                continue
 
-    seed: int = np.random.seed(input['SEED']) if input['SEED'] else np.random
-    arrival = Path("utilities")
+            key, value = line.strip().split("=")
+            config_dict[key.strip()] = value.strip()
 
-    true_path = arrival/"output_maze.txt"
-    with open(true_path, 'w', encoding='utf-8') as file:
-        file.write()
+    #diccionario temporal, luego se usara input con uno valido parseado
+    width = config_dict['WIDTH']
+    height = config_dict['HEIGHT']
+    entry = config_dict['ENTRY']
+    exit = config_dict['EXIT']
+    output_file = config_dict['OUTPUT_FILE']
+    perfect = config_dict['PERFECT']
 
-
-
+    #caracteres para el display del laberinto en terminal
+    EMPTY = ' '
+    MARK = '@'
+    WALL = chr(9608)
+    NORTH, SOUTH, EAST, WEST = n, s, e, w
 
     return "utilities/output_maze.txt"
+
+
+generate_maze("/home/caperale/milestone2/amazeing/utilities/config.txt")
