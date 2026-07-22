d99e1ef1f250852100afd218f9016b2d27c1de41
Armando Galvan De Castro <agalvan-@c1r17s4.42madrid.com>
2026-07-22 18:16:43 +0200

    main structure

--- a_maze_ing.py
+++ a_maze_ing.py
@@ -12,11 +12,19 @@ def main() -> None:
     except FileNotFoundError as e:
         print(f"Not the best path? - {e}")
         sys.exit(1)
-    try:
-        struc.validate_maze(struc.generate_maze(data), data)
-    except FileNotFoundError as e:
-        print(f"Not the best path? - {e}")
-        sys.exit(1)
+    with open(struc.generate_maze(data), 'r', encoding='utf-8') as file:
+            raw_data = [line.strip() for line in file]
+
+    while True:
+        match input("Terminal rendering (1) or MLX library (2), exit(3)"):
+            case 1:
+                print()
+            case 2:
+                print()
+            case 3:
+                sys.exit("Exiting...")
+            case _:
+                raise ValueError("Not a valid option")
 
 
 if __name__ == "__main__":
--- structure/parsing.py
+++ structure/parsing.py
@@ -61,9 +61,12 @@ def validate_conf(filepath: str) -> dict:
         print(f"Validation error: {e}")
         sys.exit(1)
 
+
+#Realmente hay que validarlo?
+# El algoritmo de generacion deberia de ser bueno de por si
 def validate_maze(filepath: str, data: dict) -> bool:
     with open(filepath, 'r', encoding='utf-8') as file:
         raw_data = [line.strip() for line in file]
     if data['perfect'] == True:
-        
+
 
