5d6000f7a645906e2d5209abf0aca42225b124d6
Armando <a.galvandecastro@gmail.com>
2026-08-18 18:01:10 +0200

    refactor

--- structure/__init__.py
+++ structure/__init__.py
@@ -1,11 +1,7 @@
 from .parsing import validate_conf
 from .maze_solver import MazeSolver
-<<<<<<< HEAD
 from .maze_model import MazeModel
-=======
-from .MazeModel import MazeModel
 
->>>>>>> f62350a (changed maze_generator to go according to reusability requirements, refactored dependencies on it)
 __all__ = [
     "validate_conf",
     "MazeSolver",
